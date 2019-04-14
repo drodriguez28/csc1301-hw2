8f9e0dfe8cb61b5912b575e3fc3e9c2c35940417
Deivid Rodriguez <34578919+drodriguez28@users.noreply.github.com>
2019-04-13 23:16:10 -0400

    Update Guess.java

--- Guess.java
+++ Guess.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+
+
+public class Guess {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int usersGuess = 0;
+        
+        while (usersGuess != -1) {
+            System.out.println("Guess a number between 1-50 (or –1 to stop):");
+            usersGuess = sc.nextInt();
+            // if the usersGuess is 17 tell its correct
+            if (usersGuess == 17) {
+                System.out.println("Correct");
+                // usersGuess= -1;
+                break;
+            } else {
+                System.out.println("Inncorrect");
+            }
+        }
+    }
+}
 
