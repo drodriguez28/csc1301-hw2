bce84a61dababb3427fd99b635c684cdf9c75029
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-04-16 21:35:04 -0400

    initial checkin

--- Gameboy3.java
+++ Gameboy3.java
@@ -1,6 +1,8 @@
-
-
-
+/*
+write a program that allows the user to play a simple guessing game in which your program thinks up an 
+integer and allows the user to make guesses until the user gets it right. For each incorrect guess you will tell 
+the user whether the right answer is higher or lower. 
+*/
 
 
 
@@ -19,7 +21,7 @@ public class Gameboy3 {
         int usersGuess = 0;
         
         while (usersGuess != -1) {
-            System.out.println("Guess a number between 1-20 (or –1 to stop):");
+            System.out.println("Guess a number between 1-50 (or –1 to stop):");
             usersGuess = sc.nextInt();
             // if the usersGuess is 17 tell its correct
             if (usersGuess == 17) {
