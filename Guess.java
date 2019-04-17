e91fd93d904491bab2e90a777b58679fb6da45fb
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-04-17 09:17:59 -0400

    initial checkin

--- Guess.java
+++ Guess.java
@@ -1,26 +1,30 @@
 import java.util.Scanner;
-
+import java.util.Random;
 
 public class Guess {
+
+    public static final int MAXIUM_NUMBER = 100;
+
     public static void instructions() {
         System.out.println("This program allows you to play a guessing game");
-        System.out.println("I will think of a number between 1 and 100");
+        System.out.println("I will think of a number between 1 and " + MAXIUM_NUMBER);
         System.out.println("and will allow you to guess until you get it ");
         System.out.println(" For each guess, I will tell you whether the");
         System.out.println("right answer is higher or lower than your guess ");
     }
-    public static void main(String[] args) {
-        instructions();
-        
+
+    public static void playGame() {
+        Random rand = new Random();
+        int answer = rand.nextInt(MAXIUM_NUMBER) + 1;
+        System.out.println("random number " + answer);
+
         Scanner sc = new Scanner(System.in);
-        
         int usersGuess = 0;
-        
         while (usersGuess != -1) {
-            System.out.println("Guess a number between 1-50 (or –1 to stop):");
+            // System.out.println("Guess a number between 1-50 (or –1 to stop):");
             usersGuess = sc.nextInt();
             // if the usersGuess is 17 tell its correct
-            if (usersGuess == 17) {
+            if (usersGuess == answer) {
                 System.out.println("Correct");
                 // usersGuess= -1;
                 break;
@@ -28,5 +32,11 @@ public class Guess {
                 System.out.println("Inncorrect");
             }
         }
+    }
+
+    public static void main(String[] args) {
+        instructions();
+        playGame();
+
     }
 }
