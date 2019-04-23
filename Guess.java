daadc77d80abc29e315cfc12f686d524552db594
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-04-22 22:32:03 -0400

    hello

--- Guess.java
+++ Guess.java
@@ -13,19 +13,20 @@ public class Guess {
         System.out.println("right answer is higher or lower than your guess ");
     }
 
-    public static void playGame() {
+    public static int playGame() {
         Random rand = new Random();
+        // answer holds the random number for the user to guess it
         int answer = rand.nextInt(MAXIMUM_NUMBER) + 1;
 
         System.out.println("I'm thinking of a number... ");
 
         Scanner sc = new Scanner(System.in);
-
+        // is to hold the guess that the users makes
         int usersGuess = 0;
 
-        // creating a counter was to hold the numbers of attempts
+        // creating a counter was to hold the numbers of guesses
         // setting it to zero because, the player hasn't started makng guesses
-        int counter = 0;
+        int counterNumbersGuesses = 0;
 
         while (usersGuess != answer) {
             // while the user is guesssing inncorectly, its telling the player to go Higher
@@ -43,16 +44,30 @@ public class Guess {
                 System.out.println("lower");
             }
             // the counter below keeps up with, the number of trys they attempted
-            counter++;
+            counterNumbersGuesses++;
 
         }
-        System.out.println("You got it right in " + counter + " guesses");
+        System.out.println("You got it right in " + counterNumbersGuesses + " guesses");
+        return counterNumbersGuesses;
+
         // System.out.println("Do you want to play again?");
     }
 
     public static void main(String[] args) {
-        //instructions();
-        //playGame();
+        instructions();
+        // records LAST game results
+        int resultsLastGame = playGame();
+        // records Games played- already played ONE game
+        int counterGamesPlayed = 1;
+        // ALL GUESS FROM ALL GAMES
+        int totalNumberGuesses = 0;
+        totalNumberGuesses = totalNumberGuesses + resultsLastGame;
+        //System.out.println(" " + resultsLastGame);
+        // record worst game 
+        int maxGuessGame = 0;
+        if (resultsLastGame > maxGuessGame ) {
+            maxGuessGame=resultsLastGame;
+        }
 
         while (true) {
             Scanner sc = new Scanner(System.in);
@@ -64,7 +79,11 @@ public class Guess {
             if (usersInput.startsWith("Y") || usersInput.startsWith("y")) {
                 System.out.print("");
                 // uses this in order to excute a Yes
-                playGame();
+                resultsLastGame = playGame();
+                totalNumberGuesses = totalNumberGuesses + resultsLastGame;
+                if (resultsLastGame > maxGuessGame ) {
+                    maxGuessGame=resultsLastGame;
+                }
 
             }
 
@@ -75,18 +94,32 @@ public class Guess {
                 break;
                 //
             }
+            counterGamesPlayed++;
+            // totalNumberGuesses
             // counter += userGuess;
             // return counter;
+
         }
+
+        results(counterGamesPlayed, totalNumberGuesses, maxGuessGame);
     }
     // put in results in here
 
-    public static void results(int playGame, int counter) {
+    public static void results(int gamesPlayed, int totalGuesses, int maxGuessGame) {
         System.out.println("Overall results:");
-        System.out.println("Total games = " + playGame);
-        System.out.println("Total guesses = " + counter);
-        System.out.println("Guesses/game = (float) total/playGame");
-        System.out.println("Best game = " );
+        System.out.println("    total games   = " + gamesPlayed);
+        System.out.println("    total guesses = " + totalGuesses);
+        System.out.println("    guesses/game  = " + (float) totalGuesses/gamesPlayed);
+        System.out.println("    max guesses   = " + maxGuessGame);
 
     }
 }
+
+// the name called has to match the method definition
+// same number of parameters must match
+// the type of each parameter must match in order
+// three phases of a counter
+// 1. initi/zation (create the counter variable)- set the default value -int
+// counter =0 (before the while loop)
+// 2. modify (++1) counter = counter +1 || counter++ - in the loop
+// 3. use the final value - after the while loop
