a3df5e8657fb2e149c60e5899bd1e0f1a81fe505
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2019-04-22 22:33:02 -0400

    habibi

--- TryMe.java
+++ TryMe.java
@@ -8,35 +8,29 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class TryMe {
-    static void instructions() {
-        System.out.println("This program allows you to play a guessing game");
-        System.out.println("I will think of a number between 1 and 100");
-        System.out.println("and will allow you to guess until you get it ");
-        System.out.println(" For each guess, I will tell you whether the");
-        System.out.println("right answer is higher or lower than your guess ");
-    }
-
-    public static void numberToGuess() {
-        // TryMe rand = new TryMe();
-        // Scanner console = new Scanner(System.in);
-        // System.out.print("Your guess: ");
-        // int numberToGuess = console.nextInt(50) + 1;
-        // Random rand = new Random();
-        // int number = rand.nextInt(100)+1;
-        // System.out.println("random number "+ number );
-
-    }
-    public static void playGame() {
-        do{
-            
-        }
-         
+    
+    public static int playGame() {
+        System.out.println("starting a game");
+        int counterNumbersGuesses = 10;
+        System.out.println("number of guesses needed for this game " + counterNumbersGuesses );
+        
+       return counterNumbersGuesses;  
     }
 
     public static void main(String[] args) {
-        //instructions();
-        numberToGuess();
-        playAgain();
+        int totalNumberGuesses = 0;
+        
+        
+        System.out.println("starting the app");
+      //10  
+         
+        int resultsLastGame = playGame();
+        totalNumberGuesses = totalNumberGuesses + resultsLastGame;
+        System.out.println("total number of guesses for all games " + totalNumberGuesses);
+    
+        
+
+
 
     }
 }
